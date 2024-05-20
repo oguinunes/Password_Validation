ffda99b47059f5a6c4334dd0bf111cfb7e6cc8b5
Guilherme <pokecontgo@hotmail.com>
2024-05-20 16:33:32 -0300

    Validação de senhas

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const passwordInput = document.querySelector ("#passwordInput")
+
+passwordInput.addEventListener('input', function (){
+
+    const password = this.value //getting user password
+
+    //down here getting elements
+    const strengthIndcator = document.querySelector("#password-strength-indicator")
+
+    const strengthText = document.querySelector("#password-strength-text")
+
+    const strengths ={
+        0: "Muito Fraca",
+        1: "Fraca",
+        2: "Moderada",
+        3: "Forte",
+        4: "Muito Forte"
+    }
+
+    let score = 0
+
+    //requirements
+    if(password.length >= 8) score++
+    if(password.match(/[a-z]/))score++
+    if(password.match(/[A-Z]/))score++
+    if(password.match(/[0-9]/))score++
+    if(password.match(/[^a-z-A-Z-0-9]/))score++
+
+    //calc %, cause de width wiil be in %
+    const width = (score/4) * 100
+
+    strengthIndcator.style.width = `${width}%`//color the % 
+
+switch(score) {
+    case 1:
+        strengthIndcator.style.backgroundcolor = "#EE3838"
+        break
+    case 2:
+       strengthIndcator.style.backgroundColor = "#FFB74D"
+        break
+    case 3:
+        strengthIndcator.style.backgroundColor = "#FFF176"
+        break
+    case 4:
+        strengthIndcator.style.backgroundColor = "#81C784"
+        break
+    default:
+        strengthIndcator.style.backgroundcolor = "transparent"
+        break
+}
+
+    if(password.length > 0 ?? score> 4){
+       strengthText.innerHTML = `Senha ${strengths[score]}`
+    }else{
+        strengthText.innerHTML = ""
+    }
+})
